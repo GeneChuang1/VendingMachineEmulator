ee5407fafc572fe08b0021d94700ace85552d836
Gene Chuang <gc.genechuang@gmail.com>
2016-07-11 12:14:34 -0400

    I added src/com/gene/test/ClassIAddedJustToGetAUnitTestUp_Test.java

---
+++ src/com/gene/test/ClassIAddedJustToGetAUnitTestUp_Test.java
@@ -0,0 +1,24 @@
+package com.gene.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ClassIAddedJustToGetAUnitTestUp_Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
