1fc9eeaafc159dba9a9b40361a863d28b02cf11f
Gene Chuang <gc.genechuang@gmail.com>
2016-07-11 11:25:20 -0400

    I added subtract() method to ClassIAddedJustToGetAUnitTestUp.java

--- src/com/gene/sample/ClassIAddedJustToGetAUnitTestUp.java
+++ src/com/gene/sample/ClassIAddedJustToGetAUnitTestUp.java
@@ -5,4 +5,8 @@ public class ClassIAddedJustToGetAUnitTestUp {
 	public int add (int numOne, int numTwo){
 		return numOne+numTwo;
 	}
+	
+	public int subtract (int numOne, int numTwo){
+		return numOne-numTwo;
+	}
 }
